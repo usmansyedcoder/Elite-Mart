77800cfe2a84e769c126ea4f4c657df45d56b70b
Muhammad Usman <usmansyed.peshawar.com>
2026-06-13 14:16:03 +0500

    UI Changed

--- src/App.jsx
+++ src/App.jsx
@@ -130,7 +130,7 @@ const categories = [
   },
 ];
 
-const WHATSAPP_NUMBER = "923157661566";
+const WHATSAPP_NUMBER = "92315766156";
 
 // Color mapping for product cards
 const getCategoryColor = (category) => {
